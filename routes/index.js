df21072cc4d40ae3b9cb19b68cbd29a1d7fd14b1
Mishal <mishala@Mishals-MacBook-Pro.local>
2021-06-17 20:33:18 -0400

    update for the front and back end

--- routes/index.js
+++ routes/index.js
@@ -8,20 +8,34 @@ let fetch = require('node-fetch');
 
 // my token: Y438SYXG61VDMBE48DIQGVB2YJT5DUH1H7 //
 
-
-
 /* GET home page. */
 router.get('/', async function(req, res, next) {
+  
+  // let response = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xdac17f958d2ee523a2206206994597c13d831ec7&address=0xb415daaf22a53bfa51599a931e1d8cc593e3700f&tag=latest&apikey=Y438SYXG61VDMBE48DIQGVB2YJT5DUH1H7')
+  // let body = await response.json()
+  // console.log(response)
+  // console.log(body)
+  console.log(req.body);
+  res.render('submit.ejs');
+});
 
-  // let response = await fetch('https://etherscan.io/token/0xdac17f958d2ee523a2206206994597c13d831ec7?a=0xb415daaf22a53bfa51599a931e1d8cc593e3700f')
-  // let response = await fetch('https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0x57d90b64a1a57749b0f932f1a3395792e12e7055&apikey=Y438SYXG61VDMBE48DIQGVB2YJT5DUH1H7')
-  let response = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xdac17f958d2ee523a2206206994597c13d831ec7&address=0xb415daaf22a53bfa51599a931e1d8cc593e3700f&tag=latest&apikey=Y438SYXG61VDMBE48DIQGVB2YJT5DUH1H7')
-  let body = await response.json()
-  console.log(response)
-  console.log(body)
+
+router.get('/token', async function(req, res, next) {
+  console.log(req.query.contract)
+  console.log(req.query.address)
+  let response = await fetch(`https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=${req.query.contract}&address=${req.query.address}&tag=latest&apikey=Y438SYXG61VDMBE48DIQGVB2YJT5DUH1H7`)
+  let body = await response.json();
   
   res.render('index', { body: body.result });
 });
+
+router.get('/sample', async function(req, res, next) {
+  let response = await fetch('https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=0xdac17f958d2ee523a2206206994597c13d831ec7&address=0xb415daaf22a53bfa51599a931e1d8cc593e3700f&tag=latest&apikey=Y438SYXG61VDMBE48DIQGVB2YJT5DUH1H7')
+  let body = await response.json();
+
+  res.render('index', { body: body.result });
+});
+
 
 
 module.exports = router;
